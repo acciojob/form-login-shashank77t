53d863cb54c5b6008706db298cfe894568a09902
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-17 09:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function getFormvalue() {
-    //Write your code here
+
+  const inputs=document.querySelectorAll('input');
+
+function getFormvalue(e) {
+   e.preventDefault();
+	let firstname=inputs[0].value;
+	let secondname=inputs[1].value;
+	let s=firstname+" "+secondname;
+	alert(s);
+	
 
 }
+inputs[2].addEventListener("click",getFormvalue);
+
+
+
+
+
